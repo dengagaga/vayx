aa69ad4bccbb3c180f36b7de6369d955f3d5d0c6
dengagaga <du.nefyodov@gmail.com>
2025-04-23 22:49:34 +0300

    two

---
+++ js/animation.js
@@ -0,0 +1,20 @@
+const animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-animation'), // контейнер для анимации
+    renderer: 'svg', // тип рендерера (может быть 'svg', 'canvas' или 'html')
+    loop: false, // зацикливание анимации
+    autoplay: false, // автоматический запуск анимации
+    path: './js/data.json',
+    
+  });
+
+  function start() {
+    animation.play();
+    
+  }
+  function stop() {
+    animation.stop(); 
+    
+  }
+
+  document.getElementById('lottie-animation').addEventListener('mouseover', start);
+  document.getElementById('lottie-animation').addEventListener('mouseout', stop);
